b3208881cec4fc5befa244258089560a7cb2e194
Nayeema <nstonny@gmail.com>
2017-11-28 17:08:11 +0100

    deleted public folder

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-// app.get('/', function(req, res){
-//     res.send('Hello express');
-// });
+app.get('/', function(req, res){
+    res.send('Hello express');
+});
 
-app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function(){
     console.log('Express server started');
