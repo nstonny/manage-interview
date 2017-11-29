21a95417d7a11c69d31ab9833e1b3eb9aa37177b
Nayeema <nstonny@gmail.com>
2017-11-29 23:00:46 +0100

    Added GET timeslots based on candidate id

--- server.js
+++ server.js
@@ -305,31 +305,29 @@ app.put('/candidates/:id', function(req,res){
 //..................... end of PUT requests .........................
 
 //....................GET time slots by candidates id...............
-
 app.get('/timeslots/:id', function(req,res){
     var candidateId = parseInt(req.params.id);
     var where = {};
 
     db.candidate.findById(candidateId).then(function(candidate){
-        if(!!candidate){
+        if(!!candidate){            
+            var string = candidate.managers;
+            candidate.managers = string.replace(/, +/g, ",").split(",").map(Number);
             where.employeeId ={
                 $in: [candidate.managers]
             };
             db.availability.findAll({
                 where: where
-
             }).then(function (availabilities) {
                 res.json(availabilities);
             });
-
-
         }else{
-            res.status(404).send();
+            res.status(404).send({
+                "error" : "No available time slot for next week"
+            });
         }
-
     }, function(e){
         res.status(500).send();
-
     });
 });
 //....................end of GET time slots by candidates id
