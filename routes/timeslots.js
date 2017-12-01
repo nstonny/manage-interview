010c78ce88dbae4db00ac2885c6a5055044cd365
Nayeema <nstonny@gmail.com>
2017-12-01 13:01:21 +0100

    re-factored to use timeslot route file

--- routes/timeslots.js
+++ routes/timeslots.js
@@ -1,36 +1,40 @@
 
-//....................GET time slots by candidates id...............
-app.get('/timeslots/:id', function(req,res){
-    var candidateId = parseInt(req.params.id);
-    var where = {};
+var express = require('express');
+var router = express.Router();
+var route = router.route('/:id');
+var db = require('../db.js');
 
-    db.candidate.findById(candidateId).then(function(candidate){
-        if(!!candidate){            
-            var string = candidate.managers;
-            candidate.managers = string.replace(/, +/g, ",").split(",").map(Number);
-            where.employeeId ={
-                $in: [candidate.managers]
-            };
-            db.availability.findAll({
-                where: where
-            }).then(function (availabilities) {
-                if(availabilities.length === 0){
-                    res.status(404).send({
-                        "error": "No available time slot for next week for this candidate"
-                    })
-                }else{
-                    res.json(availabilities);
-                }
-            });
-        }else{
-            res.status(404).send({
-                "error" : "No candidate available"
-            });
-        }
-    }, function(e){
-        res.status(500).send();
+route
+    .get(function (req, res) {
+        var candidateId = parseInt(req.params.id);
+        var where = {};
+        db.candidate.findById(candidateId).then(function (candidate) {
+            if (!!candidate) {
+                var string = candidate.managers;
+                candidate.managers = string.replace(/, +/g, ",").split(",").map(Number);
+                where.employeeId = {
+                    $in: [candidate.managers]
+                };
+                db.availability.findAll({
+                    where: where
+                }).then(function (availabilities) {
+                    if (availabilities.length === 0) {
+                        res.status(404).send({
+                            "error": "No available time slot for next week for this candidate"
+                        })
+                    } else {
+                        res.json(availabilities);
+                    }
+                });
+            } else {
+                res.status(404).send({
+                    "error": "No candidate available"
+                });
+            }
+        }, function (e) {
+            res.status(500).send();
+        });
     });
-});
-//....................end of GET time slots by candidates id
 
+module.exports = router;
 
--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.get('/', function(req, res){
 app.use('/employees', require('./routes/employee.js'));
 app.use('/candidates', require('./routes/candidate.js'));
 app.use('/availabilities', require('./routes/availabilities.js'));
+app.use('/timeslots', require('./routes/timeslots.js'));
 
 db.sequelize.sync({
+    //force:true
     }).then(function(){
     app.listen(PORT, function(){
         console.log('Express listening on port ' + PORT + '!');
