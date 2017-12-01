473dd2a816a5bcebce455ed0564c1650aad717db
Nayeema <nstonny@gmail.com>
2017-12-01 12:27:03 +0100

    re-factored to separate employee route file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res){
 }); 
 
 app.use('/employees', require('./routes/employee.js'));
-
+app.use('/candidates', require('./routes/candidate.js'));
 
 db.sequelize.sync({
     //force:true
