097e0eae2a0dd1977999de4af8facc0b4e9df663
Nayeema <nstonny@gmail.com>
2017-12-03 15:59:09 +0100

    added local sqlite database

--- models/user.js
+++ models/user.js
@@ -87,7 +87,7 @@ module.exports = function (sequelize, DataTypes) {
             instanceMethods: {
                 toPublicJSON: function () {
                     var json = this.toJSON();
-                    return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+                    return _.pick(json, 'id', 'email');
                 },
                 generateToken: function(type){
                     if(!_.isString(type)){
